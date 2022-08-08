a965f774ed1201a0c62327f51cf52beafeedb22b
yamataku29 <tmn.sby001@gmail.com>
2022-08-08 22:04:01 +0900

    犬を追加出来る様に修正

--- RealmSample/Controllers/DogListViewController.swift
+++ RealmSample/Controllers/DogListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class DogListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
@@ -23,6 +24,7 @@ class DogListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        setNavigationBarItem()
     }
 }
 
@@ -31,6 +33,56 @@ private extension DogListViewController {
         tableView.delegate = self
         tableView.dataSource = self
         navigationItem.title = person?.name
+    }
+    
+    func setNavigationBarItem() {
+        let addBarButtonItem = UIBarButtonItem(title: "＋追加", style: .done, target: self, action: #selector(tapAddButton))
+        navigationItem.rightBarButtonItem = addBarButtonItem
+    }
+    
+    func add(dog name: String) {
+        let realm = try? Realm()
+        try? realm?.write {
+            let newDog = Dog()
+            newDog.name = name
+            person?.dogs.append(newDog)
+        }
+    }
+    
+    func dataReload() {
+        let realm = try? Realm()
+        guard let id = person?.id,
+              let result = realm?.objects(Person.self).filter("id == %@", id).first else { return }
+        person = result
+        tableView.reloadData()
+    }
+}
+
+@objc extension DogListViewController {
+    func tapAddButton() {
+        let alert = UIAlertController(title:"犬の名前を入力してください",
+                                      message: "",
+                                      preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: { (action: UIAlertAction!) -> Void in
+        })
+        let defaultAction = UIAlertAction(title: "Add",
+                                          style: .default,
+                                          handler: { (action: UIAlertAction!) -> Void in
+            // UIAlertController上のUITextFidleから文字列を取得
+            guard let text = alert.textFields?.first?.text else { return }
+            self.add(dog: text)
+            self.dataReload()
+        })
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        
+        // UIAlertControllerに表示するUITextField
+        alert.addTextField(configurationHandler: {(text: UITextField!) -> Void in
+        })
+        present(alert, animated: true)
     }
 }
 
