5a1d25b66d653d2adaa6ae70610b02b2ab2e637b
yamataku29 <tmn.sby001@gmail.com>
2022-08-08 21:50:55 +0900

    DogListViewControllerへ画面遷移した際のクラッシュを修正

--- RealmSample/Controllers/DogListViewController.swift
+++ RealmSample/Controllers/DogListViewController.swift
@@ -14,11 +14,15 @@ class DogListViewController: UIViewController {
     
     init() {
         super.init(nibName: String(describing: DogListViewController.self), bundle: nil)
-        configure()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
     }
 }
 
