08c2f1aa2814ccf374013e1ef4c14cc84a9c8480
yamataku29 <tmn.sby001@gmail.com>
2022-08-08 22:00:04 +0900

    Personモデルにidを追加

--- RealmSample/Models/Person.swift
+++ RealmSample/Models/Person.swift
@@ -9,6 +9,10 @@ import Foundation
 import RealmSwift
 
 class Person: Object {
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    @objc dynamic var id = UUID().uuidString
     @objc dynamic var name = ""
     let dogs = List<Dog>()
 }
