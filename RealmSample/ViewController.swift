bab6259ca22a37c9623f79bb68d76a17b3a3f79d
yamataku29 <tmn.sby001@gmail.com>
2022-08-08 09:16:55 +0900

    UITableViewの設定

--- RealmSample/ViewController.swift
+++ RealmSample/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
